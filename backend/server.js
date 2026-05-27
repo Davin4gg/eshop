76df1f1d0d769eb459a9c0223567001eb9c84133
Davin4gg <Davin4gg@gmail.com>
2026-05-27 20:01:37 +0300

    3

--- backend/server.js
+++ backend/server.js
@@ -158,3 +158,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
+
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+});
