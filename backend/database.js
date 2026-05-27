b65f611cfd0f4d24c83f3d5447e27b4592f99f94
Davin4gg <Davin4gg@gmail.com>
2026-05-27 21:09:15 +0300

    7

--- backend/database.js
+++ backend/database.js
@@ -1,7 +1,7 @@
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 
-const dbPath = path.join(__dirname, 'store.db');
+const dbPath = path.join('/data', 'store.db');
 const db = new sqlite3.Database(dbPath, sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, (err) => {
     if (err) {
         console.error('Ошибка подключения к БД:', err.message);
