c02a599dc70e8e414296acbe18a81cbb01d80380
Davin4gg <Davin4gg@gmail.com>
2026-06-04 02:53:10 +0300

    mama

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use('/images', express.static('/data/images'));
+app.use('/images', express.static('/images'));
 app.use(express.static(path.join(__dirname, 'public', 'build')));
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'build', 'index.html'));
