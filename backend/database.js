e2961456762455d755c34dba0a2cde8cec3fdb8e
Davin4gg <Davin4gg@gmail.com>
2026-06-04 02:26:38 +0300

    plz

--- backend/database.js
+++ backend/database.js
@@ -1,6 +1,6 @@
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
-const dbPath = '/data/eshop_final.sqlite'; 
+const dbPath = path.join(__dirname, 'eshop_final.sqlite');
 const db = new sqlite3.Database(dbPath, (err) => {
     if (err) {
         console.error('Ошибка подключения к SQLite:', err.message);
