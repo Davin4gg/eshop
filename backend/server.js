b06c5db17b6784ea3d9f050d0146568b19e275b2
Davin4gg <Davin4gg@gmail.com>
2026-06-03 23:02:49 +0300

    12

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const db = require('./database');
 const app = express();
 const PORT = process.env.PORT || 80;
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
 
